4b8197ebe04f0647791e4b8e4218468c7c8d1ea7
minkyu1128-h <minkyu1128@gmail.com>
2025-02-06 00:56:57 +0900

    first commit

---
+++ src/main/java/com/example/examplesbackendtraffic/ExamplesBackendTrafficApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplesbackendtraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesBackendTrafficApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamplesBackendTrafficApplication.class, args);
+    }
+
+}
